73d8f62bb76695a4556f11c1440b967b4c360561
Brandon Muller <branmuller@gmail.com>
2024-07-27 18:58:17 -0500

    cetnered image and add border

--- src/app/components/full-product-page.tsx
+++ src/app/components/full-product-page.tsx
@@ -2,5 +2,17 @@ import { getProduct } from "~/server/queries";
 
 export default async function FullPageProductView(props: {productId: number}) {
   const product = await getProduct(props.productId);
-  return <img src={product.url} className="w-96" />
+  return (
+    <div className="flex w-full h-full min-w-0">
+        <div className="flex-shrink flex justify-center items-center">
+            <img src={product.url} className="flex-shrink object-contain" />
+        </div>
+
+        <div className="flex w-48 flex-col flex-shrink-0 border-l">
+            <div className="text-xl font-bold">
+                {product.name}
+            </div>
+        </div>
+    </div>
+    )
 }
