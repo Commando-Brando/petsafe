a2d7200b61fcc191e10db75f4d8db8cf9f99285b
CommandoBrando <lift.off.to.andromeda@gmail.com>
2024-08-10 10:37:07 -0500

    added spinner and styles for sonner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { getMyProducts } from "~/server/queries";
 
-export const dynamic = "force-dynamic"
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-
   const products = await getMyProducts();
 
   function Products() {
@@ -14,27 +13,30 @@ export default async function HomePage() {
       /* flex wrap means it wont stop with one row and will make multiple rows*/
       /* Gap adds spacing between images  */
       <div className="flex flex-wrap justify-center gap-4 p-4">
-        {[...products, ...products, ...products, ...products].map((product) => (
-          <div key={product.id} className="flex w-48 h-60 flex-col">
+        {products.map((product) => (
+          <div key={product.id} className="flex h-60 w-48 flex-col">
             <Link href={`/products/${product.id}`}>
               <Image
-              src={product.url}
-              alt="image" style={{objectFit: "contain"}}
-              width={480}
-              height={480}
+                src={product.url}
+                alt="image"
+                style={{ objectFit: "contain" }}
+                width={480}
+                height={480}
               />
             </Link>
             <div>{product.name}</div>
           </div>
         ))}
       </div>
-    )
+    );
   }
 
   return (
     <main className="">
       <SignedOut>
-        <div className="w-full h-full text-2xl text-center">Please sign in above</div>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
       </SignedOut>
       <SignedIn>
         <Products />
