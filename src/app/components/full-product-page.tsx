4fb5724cbe1bd1181baf9bbf9cff7ff56b257d34
CommandoBrando <lift.off.to.andromeda@gmail.com>
2024-08-10 17:03:45 -0500

    added delete button

--- src/app/components/full-product-page.tsx
+++ src/app/components/full-product-page.tsx
@@ -1,29 +1,43 @@
 import { clerkClient } from "@clerk/nextjs/server";
-import { getProduct } from "~/server/queries";
+import { deleteImage, getProduct } from "~/server/queries";
+import { Button } from "src/components/ui/button";
 
-export default async function FullPageProductView(props: {productId: number}) {
+export default async function FullPageProductView(props: {
+  productId: number;
+}) {
   const product = await getProduct(props.productId);
   const uploaderInfo = await clerkClient.users.getUser(product.userId);
 
   return (
-    <div className="flex w-full h-full min-w-0">
-        <div className="flex-shrink flex justify-center items-center">
-            <img src={product.url} className="flex-shrink object-contain" />
-        </div>
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={product.url} className="flex-shrink object-contain" />
+      </div>
 
-        <div className="flex w-48 flex-col flex-shrink-0 border-l">
-            <div className="text-lg border-b text-center p-2">
-                {product.name}
-            </div>
-            <div className="flex flex-col p-2">
-                <span>Uploaded By:</span>
-                <span>{uploaderInfo.fullName}</span>
-            </div>
-            <div className="flex flex-col p-2">
-                <span>Created On:</span>
-                <span>{new Date(product.createdAt).toLocaleDateString()}</span>
-            </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">{product.name}</div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.fullName}</span>
         </div>
+        <div className="flex flex-col p-2">
+          <span>Created On:</span>
+          <span>{new Date(product.createdAt).toLocaleDateString()}</span>
+        </div>
+        <div className="p-2">
+          <form
+            action={async () => {
+              "use server";
+
+              await deleteImage(props.productId);
+            }}
+          >
+            <Button type="submit" variant={"destructive"}>
+              Delete
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,32 +1,47 @@
-import "server-only"
+import "server-only";
 import { db } from "./db";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
+import { products } from "./db/schema";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function getMyProducts() {
-    const user = auth();
+  const user = auth();
 
-    // TODO: Fix this so that it does not just error out on homepage :(
-    if (!user.userId) {
-        return []
-        // throw new Error("Unauthorized");
-    }
+  // TODO: Fix this so that it does not just error out on homepage :(
+  if (!user.userId) {
+    return [];
+    // throw new Error("Unauthorized");
+  }
 
-    const products = await db.query.products.findMany({
-        where: (model, { eq }) => eq(model.userId, user.userId),
-        orderBy: (model, { desc }) => desc(model.id),
-    });
-    return products;
+  const products = await db.query.products.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return products;
 }
 
 export async function getProduct(id: number) {
-    const user = auth();
-    if (!user.userId) throw new Error("Unauthorized");
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
 
-    const product = await db.query.products.findFirst({
-        where: (model, { eq }) => eq(model.id, id),
-    })
+  const product = await db.query.products.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
 
-    if (!product) throw new Error("Product not found");
+  if (!product) throw new Error("Product not found");
 
-    return product
+  return product;
+}
+
+export async function deleteImage(id: number) {
+  const user = auth();
+  if (!user.userId) throw new Error("Unauthorized");
+
+  await db
+    .delete(products)
+    .where(and(eq(products.id, id), eq(products.userId, user.userId)));
+
+  redirect("/");
 }
